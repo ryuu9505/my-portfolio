9c95c133b8f8e4fb856a907fc7680d2601ee59cd
jun <ryuu.public@gmail.com>
2025-06-03 22:18:51 +0900

    style: search bar

--- src/styles/layout/SearchBarStyles.js
+++ src/styles/layout/SearchBarStyles.js
@@ -8,19 +8,20 @@ export const SearchBarContainer = styled.div`
   z-index: 1001;
   background-color: rgba(0, 0, 0, 0.8);
   overflow: hidden;
-  height: ${({ open }) => (open ? '44px' : '0')};
+  height: ${({ open }) => (open ? '256px' : '0')};
   min-height: 0;
   max-height: ${({ open }) => (open ? '100vh' : '0')};
   opacity: ${({ open }) => (open ? 1 : 0)};
   transition: height 0.3s cubic-bezier(.4,0,.2,1), opacity 0.3s cubic-bezier(.4,0,.2,1);
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   pointer-events: ${({ open }) => (open ? 'auto' : 'none')};
+  
 `;
 
 export const SearchInputWrapper = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
+  max-width: 1024px;
+  margin: 50px auto 0 auto;
   width: 100%;
   display: flex;
   align-items: center;
