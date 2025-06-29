eadaab3f2c459ddc9e044e8d7824206536c6bf95
jun <ryuu.public@gmail.com>
2025-06-29 18:04:03 +0900

    style: header

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-export default function LoginButton({ style, children = '로그인' }) {
+export default function LoginButton({ style, children = 'Sign in' }) {
   return (
     <RouterLink
       to="/login"
       style={{
         color: '#fff',
-        fontWeight: 600,
+        fontWeight: 500,
         fontSize: '1rem',
         textDecoration: 'none',
         display: 'flex',
